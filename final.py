bccb556d697f1251e965c8121f278438800720bf
Demery Gijsbers <demgijs@umich.edu>
2017-12-06 14:32:53 -0500

    most setup complete

--- final.py
+++ final.py
@@ -5,3 +5,62 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
+from threading import Thread
+from werkzeug import secure_filename
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'hardtoguessstringfromsi364thisisnotsupersecure'
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/demgijsfinal"
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587 #default
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') # TODO export to your environs -- may want a new account just for this. It's expecting gmail, not umich
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_SUBJECT_PREFIX'] = '[Tweets App]'
+app.config['MAIL_SENDER'] = 'demgijs364@gmail.com' # TODO fill in email
+app.config['ADMIN'] = os.environ.get('ADMIN')
+
+manager = Manager(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+mail = Mail(app)
+
+def make_shell_context():
+    return dict(app=app, db=db, Tweet=Tweet, User=User, Mention=Mention) 
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg]) 
+    thr.start()
+    return thr
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    db.create_all()
+    manager.run()
