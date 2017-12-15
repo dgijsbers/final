d5b3f867a561bc03fd80271965b62f70ce532da8
Demery Gijsbers <demgijs@umich.edu>
2017-12-14 23:13:33 -0500

    overhauling project

--- final.py
+++ final.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager, Shell
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField, IntegerField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
@@ -10,11 +10,6 @@ from flask_mail import Mail, Message
 from threading import Thread
 from werkzeug import secure_filename
 import requests
-import json
-import re
-import tweepy
-import twitter_info
-
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -41,17 +36,6 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 mail = Mail(app)
 
-
-#twitter authentication information 
-consumer_key = twitter_info.consumer_key
-consumer_secret = twitter_info.consumer_secret
-access_token = twitter_info.access_token
-access_token_secret = twitter_info.access_token_secret
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth, parser = tweepy.parsers.JSONParser())
-
 def make_shell_context():
     return dict(app=app, db=db, Tweet=Tweet, User=User, Mention=Mention) 
 
@@ -72,41 +56,37 @@ def send_email(to, subject, template, **kwargs):
 
 Tweet_Mention = db.Table('Tweet_Mention', db.Column('tweet_id', db.Integer, db.ForeignKey('tweet.id')), db.Column('mention_id', db.Integer, db.ForeignKey('mention.id')))
 
-class Tweet(db.Model):
-	__tablename__ = "tweet"
+class Recipe(db.Model):
+	__tablename__ = "recipe"
 	id = db.Column(db.Integer, primary_key=True)
 	text = db.Column(db.String(285))
-	user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-	twitter_username = db.Column(db.String(64), unique = True)
-	mentions = db.relationship('Mention', secondary = Tweet_Mention, backref = db.backref('tweet', lazy = 'dynamic'), lazy = 'dynamic')
-
+	
 class User(db.Model): 
 	__tablename__ = "user"
 	id = db.Column(db.Integer, primary_key = True)
-	twitter_username = db.Column(db.String(64), unique = True)
-	#in_tweets = db.relationship('Tweet', backref = "User") #the tweets the user is mentioned in 
-	in_tweets = db.Column(db.String(64)) 
+	rg_username = db.Column(db.String(64), unique = True) 
 	email_address = db.Column(db.String(64), unique = True)
 
-class Mention(db.Model):
+class Video(db.Model):
 	__tablename__ = "mention"
 	id = db.Column(db.Integer, primary_key = True)
-	mentioned = db.Column(db.String)
+	title = db.Column(db.String(64))
 	#user = db.relationship('User', backref = "Mention")
 
-class TweetForm(FlaskForm):
+class ProfileForm(FlaskForm):
 	username = StringField("Enter your Twitter handle (example '@my_username'):", validators = [Required()])
+	choice = RadioField('Are you looking for recipes or food videos?', choices=[('Recipes!','Recipes!'), ('Videos!', 'Videos!')])
 	email = StringField("Enter the email address to which you would like your mention updates to be sent:", validators = [Required()])
 	submit = SubmitField('Sign me up!')
 
 
 ##Put get_or_create functions here 
-def get_or_create_tweet(db_session, tweet_text, my_handle, tweets_im_in):
-	tweet = db.session.query(Tweet).filterby(my_handle=user_id).first()
+def get_or_create_tweet(db_session, tweet_text, my_handle):
+	tweet = db.session.query(Tweet).filter_by(user_id = my_handle).first()
 	if tweet:
 		return tweet
 	else:
-		tweet = Tweet(tweet_text = text, my_handle = user_id, tweets_im_in = mentions)
+		tweet = Tweet(tweet_text = text, my_handle = user_id)
 		db.session.add(tweet)
 		db.session.commit()
 		return tweet
@@ -131,9 +111,6 @@ def get_or_create_mention(db_session, my_handle, other_user):
 		db_session.commit()
 		return mention
 
-
-
-
 #Error routes
 @app.errorhandler(404)
 def page_not_found(e):
@@ -149,9 +126,9 @@ def index():
 	mentions = []
 	form = TweetForm()
 	if form.validate_on_submit():
-		if db.session.query(Tweet).filter_by(username = form.username.data).first():
+		if db.session.query(Tweet).filter_by(username = form.username.data).all():
 			flash("That user already has notifications set up...")
-		get_or_create_tweet(db.session, form.username.data, form.mention.data)
+		get_or_create_tweet(db.session, form.username.data, form.email.data)
 		if app.config['ADMIN']:
 			send_email(form.email.data, 'New Mention', 'mail/new_mention', tweet = form.text.data)
 		return redirect(url_for('see_all_mentions'))
