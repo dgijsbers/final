cf2d7aba4a1170f9b6d4066e4a6204737b62aed4
Demery Gijsbers <demgijs@umich.edu>
2017-12-17 19:41:29 -0500

    working on database entry

--- final.py
+++ final.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_script import Manager, Shell
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, RadioField, IntegerField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, RadioField, IntegerField, PasswordField, BooleanField, SelectMultipleField, ValidationError
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_mail import Mail, Message
@@ -12,6 +12,8 @@ from werkzeug import secure_filename
 import requests
 import json
 import unittest
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_required, logout_user, login_user, UserMixin, current_user
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -38,6 +40,11 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 mail = Mail(app)
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
+login_manager.init_app(app) # set up login manager
+
 def make_shell_context():
     return dict(app=app, db=db, User=User, Dogs=Dogs, Breed=Breed) 
 
@@ -54,16 +61,51 @@ def send_email(to, subject, template, **kwargs):
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg]) 
     thr.start()
-    print('email')
     return thr
 
+user_collection = db.Table('user_collection',db.Column('user_id', db.Integer, db.ForeignKey('dogs.id')),db.Column('collection_id',db.Integer, db.ForeignKey('personalCollections.id')))
 #Tweet_Mention = db.Table('Tweet_Mention', db.Column('tweet_id', db.Integer, db.ForeignKey('tweet.id')), db.Column('mention_id', db.Integer, db.ForeignKey('mention.id')))
 
-class User(db.Model): 
-	__tablename__ = "user"
-	id = db.Column(db.Integer, primary_key = True)
-	dod_username = db.Column(db.String(64), unique = True) 
-	email_address = db.Column(db.String(64), unique = True)
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    dod_username = db.Column(db.String(255), unique=True, index=True)
+    email_address = db.Column(db.String(64), unique=True, index=True)
+    #collection = db.relationship('PersonalCollection', backref='User')
+    password_hash = db.Column(db.String(128))
+
+class PersonalCollection(db.Model):
+    __tablename__ = "personalCollections"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    dogs = db.relationship('Dogs',secondary=user_collection,backref=db.backref('personalCollections',lazy='dynamic'),lazy='dynamic')
+
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+#class User(db.Model): 
+#	__tablename__ = "user"
+#	id = db.Column(db.Integer, primary_key = True)
+#	dod_username = db.Column(db.String(64), unique = True) 
+#	email_address = db.Column(db.String(64), unique = True)
 
 class Dogs(db.Model):
 	__tablename__ = "dogs"
@@ -78,22 +120,41 @@ class Breed(db.Model):
 	pic_id = db.Column(db.Integer)
 	#user = db.relationship('User', backref = "Mention")
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class ProfileForm(FlaskForm):
 	username = StringField("Enter your Dog-A-Day username (example 'iheartdogs33'):", validators = [Required()])
 	email = StringField("Enter the email address to which you would like your dogs to be sent:", validators = [Required()])
+	password = PasswordField('Enter a secure password:',validators=[Required(),EqualTo('password2',message="Passwords must match")])
+	password2 = PasswordField("Confirm that password - typos happen:",validators=[Required()])
 	submit = SubmitField('Sign me up!')
 
+	def validate_email(self,field):
+		if User.query.filter_by(email_address=field.data).first():
+			raise ValidationError('That email is already registered for dog emails!')
+
+	def validate_username(self,field):
+		if User.query.filter_by(dod_username=field.data).first():
+			raise ValidationError('Sorry - that username is already taken')
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[Required(), Length(1,64), Email()])
+	password = PasswordField('Password', validators=[Required()])
+	remember_me = BooleanField('Keep me logged in')
+	submit = SubmitField('Log In')
 
 ##Put get_or_create functions here
-def get_or_create_user(db_session, username, email):
-	user = db.session.query(User).filter_by(dod_username = username).first()
-	if user:
-		return user
+def get_or_create_users(db_session, username, email):
+	users = db.session.query(User).filter_by(dod_username = username).first()
+	if users:
+		return users
 	else:
-		user = User(dod_username = username, email_address = email)
-		db_session.add(user)
+		users = User(dod_username = username, email_address = email)
+		db_session.add(users)
 		db_session.commit()
-		return user
+		return users
 
 def get_or_create_dogs(db_session, picture_num, breed):
 	dogs = db.session.query(Tweet).filter_by(pic_id = picture_num).first()
@@ -116,7 +177,44 @@ def get_or_create_dogs(db_session, picture_num, breed):
 #		return breed
 
 
+# ROUTES FOR LOGGING IN/OUT
+@app.route('/login',methods=["GET","POST"])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email_address=form.email.data).first()
+		if user is not None and user.verify_password(form.password.data):
+			login_user(user, form.remember_me.data)
+			return redirect(request.args.get('next') or url_for('ProfileForm'))
+			flash('Invalid username or password.')
+			return render_template('login.html',form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flash('We logged you out.')
+	return redirect(url_for('login'))
+
+@app.route('/register',methods=["GET","POST"])
+def register():
+	form = ProfileForm()
+	if form.validate_on_submit():
+		user = User(email_address=form.email.data,dod_username=form.username.data,password_hash=form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash("You've got a Dog-A-Day login account now!")
+		return redirect(url_for('login'))
+		return render_template('register.html',form=form)
+
+@app.route('/secret')
+@login_required
+def secret():
+	return "Only registered users can do this! Try to register or contact the site admin at demgijs364@gmail.com."
+
+
 @app.route('/', methods = ['GET', 'POST'])
+#@login_required
 def profile_form():
 	simpleForm = ProfileForm()
 	return render_template('index.html', form=simpleForm)
@@ -125,6 +223,7 @@ def profile_form():
 
 
 @app.route('/result/alldogs', methods = ['GET', 'POST'])
+#@login_required
 def index():
 	#generate = Dogs.query.all()
 	#dogs = []
@@ -151,22 +250,8 @@ def index():
 		#base_url = 'https://dog.ceo/dog-api/breeds-image-random.php'
 #to, subject, template, **kwargs		
 
-#@app.route('/email', methods = ['GET', 'POST'])
-#def send_it():
-#		dogs = Dogs.query.all()
-#		form = ProfileForm
-#		if form.validate_on_submit():
-#			if db.session.query(User).filter_by(username = form.username.data).first():
-#				flash("That user already has emails set up...")
-#			else:
-#				get_or_create_dogs(db.session, form.username.data, form.email.data)
-#				if app.config['ADMIN']:
-#					print("********")
-#					send_email(form.email.data, 'New Dog Pic', 'mail/new_dog')
-#			return redirect(url_for('see_my_dogs'))
-	
-
 @app.route('/my_dogs')
+@login_required
 def see_my_dogs():
 	my_dogs = []
 	dogs = Dogs.query.all()
